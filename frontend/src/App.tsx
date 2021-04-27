519bf82b096cf8a73fe45cbfb4d418b897ad5089
Raul Neto <raulneto90@gmail.com>
2021-04-26 22:16:12 -0300

    Added frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import Dashboard from './pages/Dashboard';
+import logo from './logo.svg';
+import './App.css';
 
-import GlobalStyle from './styles/global';
-
-const App: React.FC = () => {
+function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Dashboard />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- frontend/src/pages/Dashboard/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Header from '../../components/Header';
-
-const Dashboard: React.FC = () => {
-  return <Header />;
-};
-
-export default Dashboard;
